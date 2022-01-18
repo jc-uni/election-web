e47fa30d0b2c80bd9fa71e6447dd3d5101fac0ab
jc-uni <jonas.claesson@unicus.com>
2022-01-18 15:26:53 +0100

    initial commit

---
+++ election-mvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using election_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace election_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SLmod()
+        {
+            return View();
+        }
+
+        public IActionResult SLorig()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
